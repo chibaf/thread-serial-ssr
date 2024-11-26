2cce601c3b00cc1583e9e344a2c52bf204e5b993
chibaf <chibaf@mac.com>
2024-11-27 03:18:58 +0900

    Add files via upload

--- logger_simulator_01.ino
+++ logger_simulator_01.ino
@@ -1,20 +1,27 @@
+long randNumber;
+
 void setup() {
 // initialize serial comms
   Serial.begin(19200);
   pinMode(13, OUTPUT);
 }
 
-int i=0;
+long i=0;
 float pi=3.14159;
 void loop() {
   i=i+1;
-  unsigned mtime;
+  long j1=0,j2=0;
+  unsigned long mtime;
+  if(i%100==0){j1=i+random(-5,5);}
+  else{j1=i;}
+    if(i%100==0){j2=i+random(-5,5);}
+  else{j2=i;}
   mtime=millis();
   Serial.print(mtime);
   Serial.print(",");
-  Serial.print(sin(2.0*pi*float(i)/50));
+  Serial.print(sin(2.0*pi*float(j1)/50));
   Serial.print(",");
-  Serial.print(cos(2.0*pi*float(i)/50));
+  Serial.print(cos(2.0*pi*float(j2)/50));
   Serial.print(",");
   Serial.print("0.0");
   Serial.print(",");
