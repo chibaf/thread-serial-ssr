9d90ee0a833fe01caf910945c45c0936e0eb64a3
chibaf <chibaf@mac.com>
2024-11-28 02:38:04 +0900

    Add files via upload

--- logger_simulator_01.ino
+++ logger_simulator_01.ino
@@ -7,22 +7,26 @@ void setup() {
 }
 
 long i=0,k1=0,k2=0;
-float pi=3.14159;
+float pi=3.14159,s1,s2;
 void loop() {
-  i=i+1;
   long j1=0,j2=0;
   unsigned long mtime;
-  if(i%100==0){k1=random(-5,5);j1=i+k1;}
+  i=i+1;
+  i=i%100;
+  if(i==0){k1=random(-2,2);j1=i+k1;}
   else{j1=i+k1;}
-  j1=i+k1;
-    if(i%100==0){k2=random(-5,5);j2=i+k2;}
+  if(i==0){k2=random(-2,2);j2=i+k2;}
   else{j2=i+k2;}
   mtime=millis();
   Serial.print(mtime);
   Serial.print(",");
-  Serial.print(sin(2.0*pi*float(j1)/100.));
+  s1=sin(2.0*pi*float(j1)/100.);
+  if (s1<0.0){s1=0.0;}
+  Serial.print(String(s1));
   Serial.print(",");
-  Serial.print(cos(2.0*pi*float(j2)/100.));
+  s2=cos(2.0*pi*float(j2)/100.);
+  if (s2<0.0){s2=0.0;}
+  Serial.print(String(s2));
   Serial.print(",");
   Serial.print("0.0");
   Serial.print(",");
